8a2f8fb5cfc927e178faa5ba2583adc028c0ba3e
zoroQT20 <155709657+zoroQT20@users.noreply.github.com>
2024-05-14 10:33:50 +0800

    edited esignature backend

--- src/main/java/com/rif/backend/Esignature/ESignatureController.java
+++ src/main/java/com/rif/backend/Esignature/ESignatureController.java
@@ -1,6 +1,7 @@
 package com.rif.backend.Esignature;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -26,4 +27,18 @@ public class ESignatureController {
         ESignature savedESignature = service.saveESignature(eSignature);
         return ResponseEntity.ok(savedESignature);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<byte[]> getESignature(@PathVariable Long id) {
+        System.out.println("Attempt to fetch ID: " + id);
+        ESignature eSignature = service.findById(id);
+        if (eSignature == null) {
+            System.out.println("No eSignature found for ID: " + id);
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok()
+                .contentType(MediaType.IMAGE_JPEG)
+                .body(eSignature.getESignaturePhoto());
+    }
+
 }
